e6b9d2c1bcaebab80fb67e136cc4725570d504f4
Aarya2004-ai <rya72004@gmail.com>
2026-04-26 15:34:36 +0530

    setup.py updated, fixed packaging issue

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
-# HYPHEN_DOT_E="-e ."
-# def get_requirement(file_path:str)->List[str]:
-#     requirement=[]
-#     with open(file_path) as file_obj:
-#         requirements=file_obj.readlines()
-#         requirements=[req.replace("\n", " ") for req in requirements ]
-#         if HYPHEN_DOT_E in requirements:
-#             requirements.remove(HYPHEN_DOT_E)
-#     return requirements     
-# setup(
-# name='MLopsProject',
-# version='0.0.1',
-# author='Aarya_S',
-# author_email='rya72004@gmail.com',
-# packages=get_requirement(where='src'),
-# package_dir={"":"src"},
-# install_requires=get_requirement('requirements.txt'))
-print(find_packages(where="src"))
+
+HYPHEN_DOT_E = "-e ."
+
+def get_requirement(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        for line in file_obj:
+            line = line.strip()
+            if line and not line.startswith("#") and line != HYPHEN_DOT_E:
+                requirements.append(line)
+    return requirements
+
+setup(
+    name='MLopsProject',
+    version='0.0.1',
+    author='Aarya_S',
+    author_email='rya72004@gmail.com',
+    packages=find_packages(where='src'),
+    package_dir={"": "src"},
+    install_requires=get_requirement('requirements.txt'),
+)
